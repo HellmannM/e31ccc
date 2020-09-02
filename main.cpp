1cdc3fabf4ede62930406aa9588d89d93bbc68ab
Matthias Hellmann <m.hellmann@me.com>
2020-09-02 11:26:52 +0200

    event loop

--- main.cpp
+++ main.cpp
@@ -1,47 +1,111 @@
-#include <fcntl.h>
-#include <fstream>
+#include <cstdint>
+#include <csignal> // SIGINT
+#include <fcntl.h> // O_RDONLY, O_NOCTTY
 #include <iomanip>
 #include <iostream>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#include <unistd.h> // read, close
 
-#include "button.h"
+bool running = true;
+size_t err_num = 0;
 
-#define BUFF_SIZE 256
-
-void print_buffer(uint8_t* buffer, size_t size)
+void sigint_handler(int signum)
 {
-    std::cout << std::hex << std::setfill('0');
-    for (int i=0; (16*i)<size; ++i)
-    {
-        for (int k=0; k<16; ++k)
-        {
-            if ( k % 4 == 0 ) std::cout << ' ';
-            std::cout << std::setw(2) << (unsigned int) buffer[16*i + k];
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::dec << std::endl;
+    err_num = signum;
+    running = false;
 }
 
 int main()
 {
+    // set signal callback
+    signal(SIGINT, sigint_handler);
+
     std::string port = "/dev/ttyS8";
     int device = open(port.c_str(), O_RDONLY | O_NOCTTY);
 
-    uint8_t buff[BUFF_SIZE];
+    uint8_t buff;
     int bytes = 0;
-    while ( bytes < BUFF_SIZE )
+    while (running)
     {
-        bytes += read(device, buff + bytes, 1);
+        bytes = read(device, &buff, 1);
+        if (bytes > 0)
+        {
+            switch (buff)
+            {
+                case 0x7f :
+                {
+                    // 0x7f : idle. do nothing
+                    break;
+                }
+                case 0x61 :
+                {
+                    //6161 6363 7f : plus
+                    //617f : abbruch
+                    bytes = read(device, &buff, 1);
+                    if (bytes > 0)
+                    {
+                        switch (buff)
+                        {
+                            case 0x7f :
+                            { // abbruch
+                                std::cout << "x";
+                                break;
+                            }
+                            case 0x61 :
+                            { // possibly plus
+                                for (int i=0; i<2; ++i)
+                                {
+                                    bytes = read(device, &buff, 1);
+                                    if (bytes <= 0){std::cout << "ERROR read 0 bytes\n"; break;}
+                                    if (buff != 0x63) break;
+                                }
+                                std::cout << "+";
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
+                case 0xa3 :
+                {
+                    // a3a3 a3a3 : minus
+                    for (int i=0; i<3; ++i)
+                    {
+                        bytes = read(device, &buff, 1);
+                        if (bytes <= 0){std::cout << "ERROR read 0 bytes\n"; break;}
+                        if (buff != 0xa3) break;
+                    }
+                    std::cout << "-";
+                    break;
+                }
+                case 0x63 :
+                {
+                    // 633f : reset
+                    bytes = read(device, &buff, 1);
+                    if (bytes <= 0){std::cout << "ERROR read 0 bytes\n"; break;}
+                    if (buff != 0x3f) break;
+                    std::cout << "r";
+                    break;
+                }
+                case 0x3f :
+                {
+                    // 3f63 : reset
+                    bytes = read(device, &buff, 1);
+                    if (bytes <= 0){std::cout << "ERROR read 0 bytes\n"; break;}
+                    if (buff != 0x63) break;
+                    std::cout << "r";
+                    break;
+                }
+                default :
+                {
+                    std::cout << "\nunrecognized symbol: 0x"
+                        << std::hex << std::setfill('0') << std::setw(2) << (unsigned int) buff << std::endl;
+                    break;
+                }
+            }
+        }
     }
-
-    std::cout << "read " << bytes << " bytes." << std::endl;
-
-    print_buffer(buff, bytes);
 
     close(device);
 
-    return 0;
+    return err_num;
 }
