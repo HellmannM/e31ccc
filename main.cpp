d1393d5fc5debbd0adc91e32aaf6860921b7560a
Matthias Hellmann <m.hellmann@me.com>
2020-09-03 15:35:27 +0200

    bugfixes and more debug messages

--- main.cpp
+++ main.cpp
@@ -39,32 +39,33 @@ int main()
                 case 0x61 :
                 {
                     //6161 6363 7f : plus
-                    //617f : abbruch
+                    //617f : on/off
                     bytes = read(device, &buff, 1);
-                    if (bytes > 0)
+                    if (bytes <= 0){std::cout<<"read error"<<std::endl; break;}
+                    switch (buff)
                     {
-                        switch (buff)
+                        case 0x7f :
+                        { // power on/off
+                            std::cout << "p";
+                            break;
+                        }
+//TODO DEBUG
+                        case 0x63 :
+                        case 0x61 :
+                        { // possibly plus
+//                            bytes = read(device, &buff, 1);
+//                            if (bytes <= 0){std::cout << "ERROR read 0 bytes\n"; break;}
+//                            if (buff != 0x63) break;
+//                            bytes = read(device, &buff, 1);
+//                            if (bytes <= 0){std::cout << "ERROR read 0 bytes\n"; break;}
+//                            if (buff != 0x63) break;
+                            std::cout << "+";
+                            break;
+                        }
+                        default:
                         {
-                            case 0x7f :
-                            { // abbruch
-                                std::cout << "x";
-                                break;
-                            }
-                            case 0x61 :
-                            { // possibly plus
-//                                bytes = read(device, &buff, 1);
-//                                if (bytes <= 0){std::cout << "ERROR read 0 bytes\n"; break;}
-//                                if (buff != 0x63) break;
-//                                bytes = read(device, &buff, 1);
-//                                if (bytes <= 0){std::cout << "ERROR read 0 bytes\n"; break;}
-//                                if (buff != 0x63) break;
-                                std::cout << "+";
-                                break;
-                            }
-                            default:
-                            {
-                                std::cout << "\nfound 0x61 0x61 0x"<<std::hex<<(unsigned int)buff<<std::endl;
-                            }
+                            std::cout<<" found 0x61 0x"<<std::hex<<(unsigned int)buff<<std::endl;
+                            break;
                         }
                     }
                     break;
@@ -75,17 +76,17 @@ int main()
                     for (int i=0; i<3; ++i)
                     {
                         bytes = read(device, &buff, 1);
-                        if (bytes <= 0){std::cout << "ERROR read 0 bytes\n"; break;}
+                        if (bytes <= 0){std::cout<<"read error"<<std::endl; break;}
                         if (buff != 0xa3) break;
+                        if (i == 2) std::cout << "-";
                     }
-                    std::cout << "-";
                     break;
                 }
                 case 0x63 :
                 {
                     // 633f : reset
                     bytes = read(device, &buff, 1);
-                    if (bytes <= 0){std::cout << "ERROR read 0 bytes\n"; break;}
+                    if (bytes <= 0){std::cout<<"read error"<<std::endl; break;}
                     if (buff != 0x3f) break;
                     std::cout << "r";
                     break;
@@ -94,19 +95,16 @@ int main()
                 {
                     // 3f63 : reset
                     bytes = read(device, &buff, 1);
-                    if (bytes <= 0){std::cout << "ERROR read 0 bytes\n"; break;}
+                    if (bytes <= 0){std::cout<<"read error"<<std::endl; break;}
                     if (buff != 0x63) break;
                     std::cout << "r";
                     break;
                 }
-                case 0x00 :
-                {
-                    break;
-                }
                 default :
                 {
-                    std::cout << "\nunrecognized symbol: 0x"
-                        << std::hex << std::setfill('0') << std::setw(2) << (unsigned int) buff << std::endl;
+                    std::cout << "\nunexpected symbol: 0x"
+                        << std::hex << std::setfill('0') << std::setw(2)
+                        << (unsigned int) buff << std::endl;
                     break;
                 }
             }
